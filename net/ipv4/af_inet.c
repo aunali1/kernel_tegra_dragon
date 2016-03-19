d3372867005e0246e989e59b5e0c1d2f67b087e0
Jesse Gross <jesse@kernel.org>
2016-03-19 09:32:00 -0700

    UPSTREAM: ipip: Properly mark ipip GRO packets as encapsulated.
    ipip encapsulated packets can be merged together by GRO but the result
    does not have the proper GSO type set or even marked as being
    encapsulated at all. Later retransmission of these packets will likely
    fail if the device does not support ipip offloads. This is similar to
    the issue resolved in IPv6 sit in feec0cb3
    ("ipv6: gro: support sit protocol").
    
    Reported-by: Patrick Boutilier <boutilpj@ednet.ns.ca>
    Fixes: 9667e9bb ("ipip: Add gro callbacks to ipip offload")
    Tested-by: Patrick Boutilier <boutilpj@ednet.ns.ca>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Jesse Gross <jesse@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    (cherry picked from commit b8cba75bdf6a48ea4811bbefb11a94a5c7281b68)
    
    BUG=b:32515955, chromium:672642
    TEST=Compile kernel and run trybots.
    
    Change-Id: I52b5052942f4590e7c722d7323ed793a563603f6
    Signed-off-by: Ben Chan <benchan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/419340
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -1461,6 +1461,13 @@ out_unlock:
 	return err;
 }
 
+static int ipip_gro_complete(struct sk_buff *skb, int nhoff)
+{
+	skb->encapsulation = 1;
+	skb_shinfo(skb)->gso_type |= SKB_GSO_IPIP;
+	return inet_gro_complete(skb, nhoff);
+}
+
 int inet_ctl_sock_create(struct sock **sk, unsigned short family,
 			 unsigned short type, unsigned char protocol,
 			 struct net *net)
@@ -1670,7 +1677,7 @@ static struct packet_offload ip_packet_offload __read_mostly = {
 	.callbacks = {
 		.gso_segment = inet_gso_segment,
 		.gro_receive = inet_gro_receive,
-		.gro_complete = inet_gro_complete,
+		.gro_complete = ipip_gro_complete,
 	},
 };
 
